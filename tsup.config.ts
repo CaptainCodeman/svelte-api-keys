b220afce8a3bc829a42a510deb78b8c48652e8ad
Simon Green <simon@captaincodeman.com>
2024-04-10 11:07:31 -0600

    don't bundle @sveltejs/kit

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	entry: ['src/lib/index.ts'],
 	format: ['esm'],
-	external: ['firebase', 'firebase-admin'],
+	external: ['firebase', 'firebase-admin', '@sveltejs/kit'],
 	splitting: true,
 	sourcemap: false,
 	minify: true,
